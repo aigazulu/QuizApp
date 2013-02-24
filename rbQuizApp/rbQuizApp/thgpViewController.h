32246605c5990b07c2347440376db51ec1fdbbd1
zulu <zulu@labs.hiddengeniusproject.org>
2013-02-23 17:15:12 -0800

    Created IBOutlet in .h file for uilabel.

--- rbQuizApp/rbQuizApp/thgpViewController.h
+++ rbQuizApp/rbQuizApp/thgpViewController.h
@@ -9,5 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @interface thgpViewController : UIViewController
+{
+    UILabel * questionNumber;
+    UILabel * artistName;
+    UIImageView * artistPicture;
+    UILabel * question;
+    UITextField * submitAnswer;
+    UILabel * answerStatus;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *UILabelquestionNumber;
+ @property (nonatomic, retain) IBOutlet UILabel *UILabelartistName;
+@property (nonatomic, retain) UIImageView *artistPicture;
+@property (nonatomic, retain) IBOutlet UILabel * UILabelquestion;
+@property (nonatomic, retain) UITextField *answer;
+@property (nonatomic, retain) IBOutlet UILabel * UILabelanswerStatus;
+
+-(IBAction)submitButtonClicked:(id)sender;
+-(IBAction)nextQuestionButtonClicked:(id)sender;
+
+
 
 @end
